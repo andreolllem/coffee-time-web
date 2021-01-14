7392848b6d70a1ed91242d3d2613322307add730
Andre Mello <andremello@MacBook-Pro-de-Andre.local>
2021-01-14 08:13:56 -0300

    Alteração admin

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,8 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require(JSON.parse(
-  process.env.NEXT_PUBLIC_FIREBASE_CONFIG
-));
+const serviceAccount = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
