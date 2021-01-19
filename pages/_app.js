e38ce176fd73b1b8012900c8dcbe4b6c3c3c7109
Andre Mello <andremello@MacBook-Pro-de-Andre.local>
2021-01-19 13:03:46 -0300

    Alterações PWA

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,32 @@
+import Head from "next/head";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+
+const APP_NAME = "Coffee-Time";
+const APP_DESCRIPTION = "App Rede Social Familiar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta name="coffee-time" content={APP_NAME} />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+        <meta name="description" content={APP_DESCRIPTION} />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#FFFFFF" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+      </Head>
+
       <Component {...pageProps} />
     </Layout>
   );
